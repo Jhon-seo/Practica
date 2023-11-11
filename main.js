5467102470aae8e640a7a0f6e51a8f30b76403b7
Jhonatan <jhonatanduqe47@gmail.com>
2023-11-11 13:35:31 -0400

    Un codigo mas legible y organizado

--- main.js
+++ main.js
@@ -1,32 +1,41 @@
-function obtenerPersonajes(done) {
-
+// Obtener los personajes de la API
+function obtenerPersonajes(callback) {
   const url = "https://rickandmortyapi.com/api/character";
 
-  const results = fetch(url);
-  //  Usar la fetch API para obtener los datos de la API
-  results
+  fetch(url)
     .then(response => response.json())
     .then(data => {
-      done(data)
+      callback(data);
     });
 }
 
-obtenerPersonajes(data => {
+// Crear elementos HTML de los personajes y agregarlos al contenedor
+function mostrarPersonajes(data) {
+  const contenedor = document.querySelector(".row");
+
   data.results.forEach(personaje => {
-    const article = document.createRange().createContextualFragment(/*mi plantilla html*/`
-    <article class="col-4">
-    <div class="card mb-5 mt-3" style="width: 18rem;">
-      <img src="${personaje.image}" class="card-img-top"
-        alt="imagen">
-        <div class="card-body">
-          <h5 class="card-title">Nombre: ${personaje.name}</h5>
-          <p class="card-text">Estatus:${personaje.status}</p>
-          <p class="card-text">Especie:${personaje.species}</p>
-        </div>
-    </div>
-    </article> `
-    );
-    const contenedor = document.querySelector(".row");
+    const article = crearElementoPersonaje(personaje);
     contenedor.append(article);
   });
-});
+}
+
+// Crear un elemento HTML para un personaje
+function crearElementoPersonaje(personaje) {
+  const article = document.createRange().createContextualFragment(/* tu plantilla HTML */`
+    <article class="col-4">
+      <div class="card mb-5 mt-3" style="width: 18rem;">
+        <img src="${personaje.image}" class="card-img-top" alt="imagen">
+        <div class="card-body">
+          <h5 class="card-title">Nombre: ${personaje.name}</h5>
+          <p class="card-text">Estatus: ${personaje.status}</p>
+          <p class="card-text">Especie: ${personaje.species}</p>
+        </div>
+      </div>
+    </article>
+  `);
+
+  return article;
+}
+
+// Llamar a las funciones para obtener los personajes y mostrarlos en la página
+obtenerPersonajes(mostrarPersonajes);
